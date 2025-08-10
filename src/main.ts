b926d44052688edf26a2fc697a029134b4c6cff0
lofi <jsj2505@gmail.com>
2025-08-10 15:05:48 +0900

    [chore] env @nestjs/config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.DEFAULT_PORT ?? 3000);
+  console.log(`App is running on port ${process.env.DEFAULT_PORT ?? 3000}`);
 }
-bootstrap();
+void bootstrap();
